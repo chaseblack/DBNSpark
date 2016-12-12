f262656df49997e734dd27c68c769203498be5fe
chaseblack <chaseandblack@gmail.com>
2016-12-12 21:18:41 +0800

    DBN with Gaussian visibles

--- src/DBN/DBN.scala
+++ src/DBN/DBN.scala
@@ -9,12 +9,13 @@ import org.apache.spark.mllib.linalg._
 import org.apache.spark.mllib.linalg.distributed.RowMatrix
 import breeze.linalg.{Matrix => BM,CSCMatrix => BSM,DenseMatrix => BDM, Vector => BV,DenseVector => BDV,SparseVector => BSV,axpy => brzAxpy,svd => brzSvd}
 import breeze.numerics.{exp => Bexp,tanh => Btanh}
-
+import breeze.stats.distributions.Gaussian
 import scala.collection.mutable.ArrayBuffer
 import java.util.Random
 import scala.math._
 
 case class DBNParams(W: BDM[Double], vW: BDM[Double],b: BDM[Double], vb: BDM[Double], c: BDM[Double], vc: BDM[Double]) extends Serializable
+case class GaussianDBNParams(W: BDM[Double], vW: BDM[Double],b: BDM[Double], vb: BDM[Double], c: BDM[Double], vc: BDM[Double], deviat: BDM[Double], v_inv_deviat: BDM[Double]) extends Serializable
 case class DBNHyperParams(size: Array[Int], layer: Int, momentum: Double, alpha: Double) extends Serializable
 
 class DBN(private var size: Array[Int], private var layer: Int, private var momentum: Double, private var alpha: Double) extends Serializable {
@@ -29,28 +30,24 @@ class DBN(private var size: Array[Int], private var layer: Int, private var mome
     val sc = train_d.sparkContext
     val dbnconfig = DBNHyperParams(size, layer, momentum, alpha)
     // init params
+    var dbn_deviat=DBN.InitVar(size)
+    var dbn_v_inv_deviat=DBN.InitvVar(size)
     var dbn_W = DBN.InitialW(size)
     var dbn_vW = DBN.InitialvW(size)
     var dbn_b = DBN.Initialb(size)
     var dbn_vb = DBN.Initialvb(size)
     var dbn_c = DBN.Initialc(size)
     var dbn_vc = DBN.Initialvc(size)
+    
     printf("Train the first RBM.........")
-    
-    
-    
-    
-    val param0 = new DBNParams(dbn_W(0), dbn_vW(0), dbn_b(0), dbn_vb(0), dbn_c(0), dbn_vc(0))
-    val param1 = RBMtrain(train_d, opts, dbnconfig, param0)
+    val param0 = new GaussianDBNParams(dbn_W(0), dbn_vW(0), dbn_b(0), dbn_vb(0), dbn_c(0), dbn_vc(0),dbn_deviat,dbn_v_inv_deviat)
+    val param1 = GaussianRBMtrain(train_d, opts, dbnconfig, param0)
     dbn_W(0) = param1.W
     dbn_vW(0) = param1.vW
     dbn_b(0) = param1.b
     dbn_vb(0) = param1.vb
     dbn_c(0) = param1.c
     dbn_vc(0) = param1.vc
-    
-    
-    
     
     printf("Train the rest RBMs.........")
     for (i <- 2 to dbnconfig.layer - 1) {
@@ -74,7 +71,135 @@ class DBN(private var size: Array[Int], private var layer: Int, private var mome
     }
     new DBNModel(dbnconfig, dbn_W, dbn_b, dbn_c)
   }
+  
+  
+  def GaussianRBMtrain(train_t: RDD[(BDM[Double], BDM[Double])], opts: Array[Double], hyperparams: DBNHyperParams, weight: GaussianDBNParams): GaussianDBNParams = {
+    val sc = train_t.sparkContext
+    var rbm_deviat=weight.deviat
+    var rbm_v_inv_deviat=weight.v_inv_deviat
+    var rbm_W = weight.W
+    var rbm_vW = weight.vW
+    var rbm_b = weight.b
+    var rbm_vb = weight.vb
+    var rbm_c = weight.c
+    var rbm_vc = weight.vc
+    val broadcast_config = sc.broadcast(hyperparams)
+    val m = train_t.count// number of train samples
+    val batchsize = opts(0).toInt
+    val numepochs = opts(1).toInt
+    val numbatches = (m / batchsize).toInt
+    
+    for (i <- 1 to numepochs) {
+      val splitW2 = Array.fill(numbatches)(1.0 / numbatches)//if numbatches=10, then Array(0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
+      var err = 0.0
+      for (l <- 1 to numbatches) {//not paralleled, a paralleled one should be compute each batch simultaneously
+        val broadcast_rbm_W = sc.broadcast(rbm_W)//num.of.layer_latter * num.of.layer_former 
+        val broadcast_rbm_vW = sc.broadcast(rbm_vW)//num.of.layer_latter * num.of.layer_former 
+        val broadcast_rbm_b = sc.broadcast(rbm_b)//col vector
+        val broadcastc_rbm_vb = sc.broadcast(rbm_vb)//col vector
+        val broadcast_rbm_c = sc.broadcast(rbm_c)//col vector
+        val broadcast_rbm_vc = sc.broadcast(rbm_vc)//col vector
+        val broadcast_rbm_deviat = sc.broadcast(rbm_deviat)//col vector
+        val broadcast_rbm_v_inv_deviat = sc.broadcast(rbm_v_inv_deviat)//col vector
+        //val broadcast_rbm_deviat=broadcast_rbm_deviat.value.map { x => sqrt(x) }
+        
+        val train_split2 = train_t.randomSplit(splitW2, System.nanoTime())/**train_split2 has numbatches batches*/
+        val batch_xy1 = train_split2(l - 1)//batch_xy1 is RDD[row vector, row vector] mathematically eventhough it is shown as RDD[BDM,BDM]
+        val batch_vh1 = batch_xy1.map { f =>// feed forward
+          val label = f._1//label is row vector, even though it is shown as BDM
+          val v0 = f._2//v1 is row vector, even though it is shown as BDM
+          //println("v0 row="+v0.rows+" v0 col="+v0.cols)
+          //println("rbm_deviat row="+broadcast_rbm_deviat.value.t.rows+" rbm_deviat col="+broadcast_rbm_deviat.value.t.cols)
+          //println("W row="+broadcast_rbm_W.value.t.rows+" W col="+broadcast_rbm_W.value.t.cols)
+          //println("rbm_c row="+broadcast_rbm_c.value.t.rows+" rbm_c cols"+broadcast_rbm_c.value.t.cols)
+          val h0 = DBN.sigm(((v0:/broadcast_rbm_deviat.value.t) * broadcast_rbm_W.value.t + broadcast_rbm_c.value.t))//row vector
+          var h_states=DBN.samplestates(h0)//row vector
+          var v1 = h_states * broadcast_rbm_W.value:*broadcast_rbm_deviat.value.t + broadcast_rbm_b.value.t + BDM.rand(1,broadcast_rbm_deviat.value.rows):*broadcast_rbm_deviat.value.t//v1 row vector
+          var h1 = DBN.sigm((v1:/broadcast_rbm_deviat.value.t) * broadcast_rbm_W.value.t + broadcast_rbm_c.value.t)//row vector
+          for(i<-2 to opts(2).toInt){//k_CD
+            h_states=DBN.samplestates(h0)
+            v1 = h_states * broadcast_rbm_W.value:*broadcast_rbm_deviat.value.t + broadcast_rbm_b.value.t+BDM.rand(1,broadcast_rbm_deviat.value.rows):*broadcast_rbm_deviat.value.t//v1 row vector
+            h1 = DBN.sigm((v1:/broadcast_rbm_deviat.value.t) * broadcast_rbm_W.value.t + broadcast_rbm_c.value.t)//row vector
+          }
+          val c1 = h0.t * (v0:/broadcast_rbm_deviat.value.t)//delta matrix contributed by ONE SINGLE training sample
+          val c2 = h1.t * (v1:/broadcast_rbm_deviat.value.t)//delta matrix contributed by ONE SINGLE training sample
+          val d1 =  v0:*(broadcast_rbm_b.value.t*2.toDouble-v0):/broadcast_rbm_deviat.value.t  +  v0:*(h0 * broadcast_rbm_W.value)//row vec
+          val d2 =  v1:*(broadcast_rbm_b.value.t*2.toDouble-v1):/broadcast_rbm_deviat.value.t  +  v1:*(h1 * broadcast_rbm_W.value)//BDM.fill[Double](1, v0.cols)(2)
+          (label, v0, h0, v1, h1, c1, c2, d1, d2)
+        }
+        
+        // UPDATE W: rbm.vW = rbm.momentum * rbm.vW + rbm.alpha * (c1 - c2) / opts.batchsize;
+        val vw1 = batch_vh1.map {/*vw1 is RDD[Matrix]*/case (label, v1, h1, v2, h2, c1, c2, d1, d2) =>(c1 - c2)}
+        val initw = BDM.zeros[Double](broadcast_rbm_W.value.rows, broadcast_rbm_W.value.cols)
+        /**c is type of U=(matrix, count), c._1 is matrix, c._2 is current count; v is type of T, i.e, delta matrix contributed by ONE SINGLE sample; 0L is zeroVal,i.e, U*/
+        val (vw2, countw2) = vw1.treeAggregate((initw, 0L))(seqOp = (c, v) => (c._1 + v, c._2 + 1),combOp = (c1, c2) => (c1._1 + c2._1, c1._2 + c2._2))
+        val vw3 = vw2 / countw2.toDouble
+        rbm_vW = broadcast_config.value.momentum * broadcast_rbm_vW.value + broadcast_config.value.alpha * vw3
+        rbm_W = broadcast_rbm_W.value + rbm_vW
+        //UPDATE  b: rbm.vb = rbm.momentum * rbm.vb + rbm.alpha * sum(v1 - v2)' / opts.batchsize; 
+        val vb1 = batch_vh1.map {case (lable, v1, h1, v2, h2, c1, c2, d1, d2) =>(v1 - v2):/broadcast_rbm_deviat.value.map{x => x*x}.t}
+        val initb = BDM.zeros[Double](broadcastc_rbm_vb.value.cols, broadcastc_rbm_vb.value.rows)
+        val (vb2, countb2) = vb1.treeAggregate((initb, 0L))(seqOp = (c, v) => (c._1 + v, c._2 + 1),combOp = (c1, c2) =>(c1._1 + c2._1, c1._2 + c2._2))
+        val vb3 = vb2 / countb2.toDouble
+        rbm_vb = broadcast_config.value.momentum * broadcastc_rbm_vb.value + broadcast_config.value.alpha * vb3.t
+        rbm_b = broadcast_rbm_b.value + rbm_vb
+        //UPDATE c: rbm.vc = rbm.momentum * rbm.vc + rbm.alpha * sum(h1 - h2)' / opts.batchsize;
+        val vc1 = batch_vh1.map {case (lable, v1, h1, v2, h2, c1, c2, d1, d2) =>(h1 - h2)}
+        val initc = BDM.zeros[Double](broadcast_rbm_vc.value.cols, broadcast_rbm_vc.value.rows)
+        val (vc2, countc2) = vc1.treeAggregate((initc, 0L))(seqOp = (c, v) =>(c._1 + v, c._2 + 1),combOp = (c1, c2) =>(c1._1 + c2._1, c1._2 + c2._2))
+        val vc3 = vc2 / countc2.toDouble
+        rbm_vc = broadcast_config.value.momentum * broadcast_rbm_vc.value + broadcast_config.value.alpha * vc3.t
+        rbm_c = broadcast_rbm_c.value + rbm_vc
+        //UPDATE var
+        val v_inv_deviat1 = batch_vh1.map {case (lable, v1, h1, v2, h2, c1, c2, d1, d2) =>(d1 - d2)}
+        val init_v_inv_deviat1 = BDM.zeros[Double](broadcast_rbm_deviat.value.cols, broadcast_rbm_deviat.value.rows)
+        val (v_inv_deviat2, count) = vc1.treeAggregate((init_v_inv_deviat1, 0L))(seqOp = (c, v) =>(c._1 + v, c._2 + 1),combOp = (c1, c2) =>(c1._1 + c2._1, c1._2 + c2._2))
+        val v_inv_deviat3=v_inv_deviat2 / count.toDouble//row vec
+        rbm_v_inv_deviat=broadcast_config.value.momentum*rbm_v_inv_deviat+v_inv_deviat3.t
+        val inv_deviat=1.toDouble/broadcast_rbm_deviat.value + rbm_v_inv_deviat
+        rbm_deviat=1.toDouble/inv_deviat
+        rbm_deviat=rbm_deviat.map { x => if(x>0.005) x else 0.005 }
 
+        val dbne1 = batch_vh1.map {// compute error
+          case (lable, v1, h1, v2, h2, c1, c2, d1, d2) =>
+            (v1 - v2)
+        }
+        val (dbne2, counte) = dbne1.treeAggregate((0.0, 0L))(
+          seqOp = (c, v) => {
+            // c: (e, count), v: (m)
+            val e1 = c._1
+            val e2 = (v :* v).sum
+            val esum = e1 + e2
+            (esum, c._2 + 1)
+          },
+          combOp = (c1, c2) => {
+            // c: (e, count)
+            val e1 = c1._1
+            val e2 = c2._1
+            val esum = e1 + e2
+            (esum, c1._2 + c2._2)
+          })
+        val dbne = dbne2 / counte.toDouble
+        err += dbne
+      }
+      printf("epoch: numepochs = %d ; Average reconstruction error is: %f.\n", i, err / numbatches.toDouble)
+    }
+    new GaussianDBNParams(rbm_W, rbm_vW, rbm_b, rbm_vb, rbm_c, rbm_vc,rbm_deviat,rbm_v_inv_deviat)
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   /***********************ATTENTION**************************************
       * Mathematically, each sample in the training set should be a DenseVector. 
       * However, in this code each sample is wrapped in a DenseMatrix, train_t. 
@@ -221,7 +346,8 @@ object DBN extends Serializable {
     val n = size.length
     val rbm_W = ArrayBuffer[BDM[Double]]()
     for (i <- 1 to n - 1) {
-      val d1 = BDM.zeros[Double](size(i), size(i - 1))
+      //val d1 = BDM.zeros[Double](size(i), size(i - 1))
+      val d1=BDM.rand(size(i), size(i - 1), new Gaussian(0,0.01))
       rbm_W += d1
     }
     rbm_W.toArray
@@ -255,7 +381,8 @@ object DBN extends Serializable {
     val n = size.length
     val rbm_b = ArrayBuffer[BDM[Double]]()
     for (i <- 1 to n - 1) {
-      val d1 = BDM.zeros[Double](size(i - 1), 1)
+      //val d1 = BDM.zeros[Double](size(i - 1), 1)
+      val d1=BDM.rand(size(i-1), 1, new Gaussian(0,0.01))
       rbm_b += d1
     }
     rbm_b.toArray
@@ -289,7 +416,8 @@ object DBN extends Serializable {
     val n = size.length
     val rbm_c = ArrayBuffer[BDM[Double]]()
     for (i <- 1 to n - 1) {
-      val d1 = BDM.zeros[Double](size(i), 1)
+      //val d1 = BDM.zeros[Double](size(i), 1)
+      val d1=BDM.rand(size(i), 1, new Gaussian(0,0.01))
       rbm_c += d1
     }
     rbm_c.toArray
@@ -311,6 +439,9 @@ object DBN extends Serializable {
     }
     rbm_vc.toArray
   }
+  // Gaussian variance
+  def InitVar(size: Array[Int]):BDM[Double]=BDM.ones[Double](size(0), 1)
+  def InitvVar(size: Array[Int]):BDM[Double]=BDM.zeros[Double](size(0),1)
   
   def samplestates(h0: BDM[Double]):BDM[Double]={
     val r1 = BDM.rand[Double](h0.rows, h0.cols)
--- src/examples/DBNApp.scala
+++ src/examples/DBNApp.scala
@@ -3,24 +3,22 @@ package examples
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import breeze.linalg.DenseMatrix
 import breeze.linalg.{DenseMatrix => BDM}
 import DBN.DBN
+import neuralnet.NeuralNet
 import breeze.linalg.{DenseMatrix => BDM}
 
 object DBNApp {
-  def main(args:Array[String]){
-    
-     val file="data.csv"
-     val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
-     val sc = new SparkContext(conf)
-     val examples = sc.textFile(file, 4).cache()
-     
-     val train_d =examples.map { line =>
+    def extract(samples:RDD[String])={
+    val a=samples.first().split(",").size-1//why "num_vislayer-1"? cause the first column is label which are excluded
+    val b=samples.map{
+     line =>
      val f1 = line.split(",")
      val f =f1.map(f =>f.toDouble)
-     val y = Array(f(1))
-     val x =f.slice(2,f.length)
+     val y = Array(f(0))
+     val x =f.slice(1,f.length)
      /***********************ATTENTION**************************************
       * Mathematically, each sample in the training set should be a DenseVector. 
       * However, in this code each sample is wrapped in a DenseMatrix. Why? I personally
@@ -33,11 +31,45 @@ object DBNApp {
      **/
      (new BDM(1,y.length,y),new BDM(1,x.length,x))
     }
+    (a,b)
+  }
+  
+  def main(args:Array[String]){
+    val file="data.csv"
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+    val sc = new SparkContext(conf)
+    val samples = sc.textFile(file, 4).cache()
+    val tuple=DBNApp.extract(samples) 
+    val num_vislayer=tuple._1
+    val train_d =tuple._2
+    val opts = Array(100.0,20.0,5)//batchsize, numepoch, k_CD 
+    val DBNmodel =new DBN().setSize(Array(num_vislayer, 7, 1)).setLayer(3).setMomentum(0.1).setAlpha(0.2).DBNtrain(train_d, opts)
     
-    val opts = Array(100.0,20.0,0.0) 
-    //3 设置训练参数，建立DBN模型
-    val DBNmodel =new DBN().setSize(Array(5, 7)).setLayer(2).setMomentum(0.1).setAlpha(0.2).DBNtrain(train_d, opts) 
-    
-    //DBNResult.show()
+    val mynn = DBNmodel.dbnunfoldtonn(1)
+    val nnopts = Array(100.0, 50.0, 0.0)
+    val numExamples = train_d.count()
+    println(s"numExamples = $numExamples.")
+    println(mynn._2)
+    for (i <- 0 to mynn._1.length - 1) {
+      print(mynn._1(i) + "\t")
+    }
+    println()
+    println("mynn_W1")
+    val tmpw1 = mynn._3(0)
+    for (i <- 0 to tmpw1.rows - 1) {
+      for (j <- 0 to tmpw1.cols - 1) {
+        print(tmpw1(i, j) + "\t")
+      }
+      println()
+    }
+    val NNmodel = new NeuralNet().setSize(mynn._1).setLayer(mynn._2).setActivation_function("sigm").setOutput_function("sigm").setInitW(mynn._3).NNtrain(train_d, nnopts)
+    //5 NN模型测试
+    val NNforecast = NNmodel.predict(train_d)
+    val NNerror = NNmodel.Loss(NNforecast)
+    println(s"NNerror = $NNerror.")
+    val printf1 = NNforecast.map(f => (f.label.data(0), f.predict_label.data(0))).take(200)
+    println("预测结果——实际值：预测值：误差")
+    for (i <- 0 until printf1.length)
+      println(printf1(i)._1 + "\t" + printf1(i)._2 + "\t" + (printf1(i)._2 - printf1(i)._1))
   }
 }
